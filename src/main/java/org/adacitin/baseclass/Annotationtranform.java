bfcf6692f6470564c252fde209920deba74e455d
Ravinandha1995 <ravinandha1995@gmail.com>
2022-11-12 12:31:48 +0530

    changes made as per Pr comment

--- src/main/java/org/adacitin/baseclass/Annotationtranform.java
+++ src/main/java/org/adacitin/baseclass/Annotationtranform.java
@@ -10,7 +10,7 @@ public class Annotationtranform implements IAnnotationTransformer{
 
 	@Override
 	public void transform(ITestAnnotation annotation, Class testClass, Constructor testConstructor, Method testMethod) {
-		// TODO Auto-generated method stub
+		
 		annotation.setRetryAnalyzer(IREtryAnalyser.class);
 	}
 
