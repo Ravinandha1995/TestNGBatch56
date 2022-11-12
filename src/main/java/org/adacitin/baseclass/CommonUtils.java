7f9e495f021e9b625f709e9b796371947a91bed6
Ravinandha1995 <ravinandha1995@gmail.com>
2022-11-12 12:39:46 +0530

    pr comments chnages made

--- src/main/java/org/adacitin/baseclass/CommonUtils.java
+++ src/main/java/org/adacitin/baseclass/CommonUtils.java
@@ -11,8 +11,6 @@ import org.slf4j.Logger;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Parameters;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
@@ -22,8 +20,6 @@ public class CommonUtils extends Setup {
 	protected static ExtentReports extentreport = new ExtentReports();
 	protected static ExtentHtmlReporter extenthtmlreport;
 	protected static ExtentTest logger;
-	
-	
 	
 	public static Logger log = Slf4J.loadLogger(CommonUtils.class);
 
